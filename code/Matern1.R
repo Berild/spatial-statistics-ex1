1673603fe5e8eff9befad6cb47b0034135478520
Unknown <karinehf@stud.ntnu.no>
2019-02-06 17:25:37 +0100

    Plotting variogram functions in Matern1

--- code/Matern1.R
+++ code/Matern1.R
@@ -1,9 +1,16 @@
+#This script plots the Matern correlation function, the Powered Exponential function
+#And the correponding variograms
+
 library(fields)
 library(geoR) #Need to install XQuartz? cov.spatial() belongs here
 library(MASS)
 library(akima)
 library(ggplot2)
-library(reshape2)
+library(reshape2)#melt
+library(gridExtra)#grid.arrange
+
+#variance, for the variogram
+sigma2 = c(1,5)
 
 distance=0:0.01:49/10
 nuMat=c(1,3)
@@ -28,6 +35,38 @@ ggplot(longDF) +
   theme(plot.title = element_text(hjust = 0.5))
 
 ggsave("../figures/corrfunc.pdf", plot = last_plot(), device = NULL, path = NULL,
-       scale = 1, width = 5.5, height = 4, units = "in",
-       dpi = 300, limitsize = TRUE)
+      scale = 1, width = 5.5, height = 4, units = "in",
+     dpi = 300, limitsize = TRUE)
 
+################################################################################
+#Plotting the variogram functions
+plots = list()
+spec = paste("Sigma^2 =", sigma2)
+
+#For some reason, the for loop wouldn't work...
+#####################################
+plots[[1]] = ggplot(longDF) + 
+    geom_line(aes(x = distance, y = sigma2[1]*(1-value), colour = variable)) + 
+    scale_colour_discrete(name = spec[1], labels = 
+                            c("Matern\n nu = 1\n", "Matern\n nu = 3\n", 
+                              "Powered exp.\n nu = 1\n", "Powered exp.\n nu = 1.9\n")) +
+    ggtitle("Variogram functions") +
+    ylab('value') +
+    theme(plot.title = element_text(hjust = 0.5))
+
+plots[[2]] = ggplot(longDF) + 
+  geom_line(aes(x = distance, y = sigma2[2]*(1-value), colour = variable)) + 
+  scale_colour_discrete(name = spec[2], labels = 
+                          c("Matern\n nu = 1\n", "Matern\n nu = 3\n", 
+                            "Powered exp.\n nu = 1\n", "Powered exp.\n nu = 1.9\n")) +
+  #ggtitle("Variogram functions") +
+  ylab('value') +
+  theme(plot.title = element_text(hjust = 0.5))
+#######################################
+
+#Save plot in one figure
+plotGrid = grid.arrange(grobs = plots, ncol = 1)
+
+ggsave("../figures/variograms.pdf", plot = plotGrid, device = NULL, path = NULL,
+      scale = 1, width = 5.5, height = 2*4, units = "in",
+     dpi = 300, limitsize = TRUE)

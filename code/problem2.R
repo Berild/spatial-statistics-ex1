b86f5265ceb9793ba87166cc09c3b3aaeb023fb7
nagasjel <moutzebe@gmail.com>
2019-02-10 23:16:03 +0100

    Update problem2.R

--- code/problem2.R
+++ code/problem2.R
@@ -12,11 +12,20 @@ library(gridExtra) #grid.arrange
 topo.data <- read.table("topo.dat", header=TRUE)
 
 # using akima library
-topo.interp <- interp(x = topo_data$x, y = topo_data$y, z = topo_data$z)
+topo.interp <- interp(x = topo.data$x, y = topo_data$y, z = topo_data$z)
 topo.zmin <- min(topo.interp$z,na.rm=TRUE)
 topo.zmax <- max(topo.interp$z,na.rm=TRUE)
-breaks <- pretty(c(topo.zmin,topo.zmax),10)
+breaks <- pretty(c(topo.zmin,topo.zmax),20)
 colors <- heat.colors(length(breaks)-1)
 with(topo.interp, image  (x,y,z, breaks=breaks, col=colors))
 with(topo.interp,contour(x,y,z, levels=breaks, add=TRUE))
 points (topo.data, pch = 3)
+
+
+
+loci <- expand.grid(seq(0,315), seq(0,315))
+krige.conv(topo.interp, locations = c(145,120), krige)
+krige.control(type.krige = "uk", trend.d = "cte", trend.l = "cte",
+             obj.model = NULL, beta, cov.model, cov.pars, kappa,
+             nugget, micro.scale = 0, dist.epsilon = 1e-10,
+             aniso.pars, lambda)

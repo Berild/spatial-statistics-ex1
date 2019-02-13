56f5fcd9c95c9ca1582d3dd0c3990ec87bd84af6
nagasjel <moutzebe@gmail.com>
2019-02-13 17:11:58 +0100

    Add multiplot functionalty and change some code
    Add alot of figures

--- code/problem2.R
+++ code/problem2.R
@@ -7,12 +7,13 @@ library(akima)
 library(ggplot2)
 library(reshape2) #melt
 library(gridExtra) #grid.arrange
+source("multiplot.R")
 
 # data
 topo.data <- read.table("topo.dat", header=TRUE)
 
 # using akima library
-topo.interp <- interp(x = topo.data$x, y = topo_data$y, z = topo_data$z)
+topo.interp <- interp(x = topo.data$x, y = topo.data$y, z = topo.data$z)
 topo.zmin <- min(topo.interp$z,na.rm=TRUE)
 topo.zmax <- max(topo.interp$z,na.rm=TRUE)
 breaks <- pretty(c(topo.zmin,topo.zmax),20)
@@ -35,7 +36,7 @@ kc2 <- krige.conv(topogeo, locations = grd, krige = krige.control(type.krige = "
 kc2.df = data.frame(x = grd[,1],y = grd[,2], pred = kc2$predict, se = sqrt(kc2$krige.var))
 
 # Plotting predicted values
-ggplot(kc2.df, aes(x=x,y=y,fill = pred)) + 
+kc2.plot.pred <- ggplot(kc2.df, aes(x=x,y=y,fill = pred)) + 
   geom_tile() + 
   scale_fill_gradient2(low = "navy",
                        mid = "yellow",
@@ -45,24 +46,28 @@ ggplot(kc2.df, aes(x=x,y=y,fill = pred)) +
                        name = "prediction")+ 
   xlab("x") +
   ylab("y") + 
-  ggtitle("UK 2nd order expecation function") +
+  ggtitle("UK 2nd order") +
   theme(plot.title = element_text(hjust = 0.5))
+kc2.plot.pred
 ggsave("../figures/uk2.pdf", plot = last_plot(), device = NULL, path = NULL,
        scale = 1, width = 5.5, height = 4, units = "in",
        dpi = 300, limitsize = TRUE)
 
 
 # Plotting variance of predicted values
-ggplot(kc2.df, aes(x=x,y=y,fill = se)) + 
+kc2.plot.se <- ggplot(kc2.df, aes(x=x,y=y,fill = se)) + 
   geom_tile() + 
-  scale_fill_gradient2(low = "yellow",
-                       high = "red",
+  scale_fill_gradient2(high = "lightpink2",
+                       mid = "steelblue2",
+                       low = "navyblue",
+                       midpoint = (max(kc2.df$se)+min(kc2.df$se))/2,
                        limits = range(kc2.df$se),
-                       name = "prediction")+ 
+                       name = "SE")+ 
   xlab("x") +
   ylab("y") + 
-  ggtitle("UK Variance 2nd order expectation function") +
+  ggtitle("UK Variance 2nd order") +
   theme(plot.title = element_text(hjust = 0.5))
+kc2.plot.se
 ggsave("../figures/uk2se.pdf", plot = last_plot(), device = NULL, path = NULL,
        scale = 1, width = 5.5, height = 4, units = "in",
        dpi = 300, limitsize = TRUE)
@@ -76,9 +81,9 @@ kc1 <- krige.conv(topogeo, locations = grd, krige = krige.control(type.krige = "
 kc1.df = data.frame(x = grd[,1],y = grd[,2], pred = kc2$predict, se = sqrt(kc2$krige.var))
 
 # Plotting predicted values
-ggplot(kc1.df, aes(x=x,y=y,fill = pred)) + 
+kc1.plot.pred <- ggplot(kc1.df, aes(x=x,y=y,fill = pred)) + 
   geom_tile() + 
-  scale_fill_gradient2(low = "navy",
+  scale_fill_gradient2(low = "navyblue",
                        mid = "yellow",
                        high = "red",
                        midpoint = (max(kc1.df$pred) + min(kc1.df$pred))/2,
@@ -86,24 +91,40 @@ ggplot(kc1.df, aes(x=x,y=y,fill = pred)) +
                        name = "prediction")+ 
   xlab("x") +
   ylab("y") + 
-  ggtitle("Universal Kriging") +
+  ggtitle("UK 1st order") +
   theme(plot.title = element_text(hjust = 0.5))
+kc1.plot.pred
 ggsave("../figures/uk1.pdf", plot = last_plot(), device = NULL, path = NULL,
        scale = 1, width = 5.5, height = 4, units = "in",
        dpi = 300, limitsize = TRUE)
 
 
 # Plotting variance of predicted values
-ggplot(kc1.df, aes(x=x,y=y,fill = se)) + 
+kc1.plot.se <- ggplot(kc1.df, aes(x=x,y=y,fill = se)) + 
   geom_tile() + 
-  scale_fill_gradient2(low = "yellow",
-                       high = "red",
+  scale_fill_gradient2(high = "lightpink2",
+                       mid = "steelblue2",
+                       low = "navyblue",
+                       midpoint = (max(kc1.df$se)+min(kc1.df$se))/2,
                        limits = range(kc1.df$se),
-                       name = "prediction")+ 
+                       name = "SE")+ 
   xlab("x") +
   ylab("y") + 
-  ggtitle("Universal Kriging Variance") +
+  ggtitle("UK Variance 1st order") +
   theme(plot.title = element_text(hjust = 0.5))
+kc1.plot.se
 ggsave("../figures/uk1se.pdf", plot = last_plot(), device = NULL, path = NULL,
        scale = 1, width = 5.5, height = 4, units = "in",
        dpi = 300, limitsize = TRUE)
+
+# multiplot of everything
+multiplot(kc1.plot.pred, kc1.plot.se, kc2.plot.pred, kc2.plot.se, cols = 2)
+ggsave("../figures/ukmulti.pdf", plot = last_plot(), device = NULL, path = NULL,
+       scale = 1, width = 5.5, height = 4, units = "in",
+       dpi = 300, limitsize = TRUE)
+
+
+# ----------------------------------------------
+# calculation of probablities d-f
+
+pnorm(700, mean = kc2.df$pred[(kc2.df$x==100)&(kc2.df$y==100)], sd = (kc2.df$se[(kc2.df$x==100)&(kc2.df$y==100)]),lower.tail = FALSE)
